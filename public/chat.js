7fea812274a0b4ed0962154d8cf4e263700ebfc5
Inffinite <louislaizer4@gmail.com>
2020-08-22 23:11:06 +0300

    :hammer: Implemented online users view on mobile

--- public/chat.js
+++ public/chat.js
@@ -2,6 +2,7 @@ const msg = document.getElementById("msg")
 const form = document.querySelector("form")
 const chatBox = document.querySelector('.chatbox')
 const activeUsers = document.querySelector('.activeUsers')
+const activeUsersMobile = document.querySelector('.users-menu')
 const tone = document.getElementById("myAudio"); 
 const {userName} = Qs.parse(location.search, {
     ignoreQueryPrefix: true
@@ -23,6 +24,7 @@ socket.on('msgFromServer', data => {
 
 socket.on('updateActiveUser', data => {
     addActiveUser(data)
+    addActiveUsersMobile(data)
 })
 
 function displayMsg(data) {
@@ -39,6 +41,23 @@ function displayMsg(data) {
         playAudio()
       }
     }
+}
+
+function addActiveUsersMobile(data) {
+    const div = document.createElement('div')
+    div.innerHTML = ""
+    for (let eachUser in data) {
+        div.innerHTML += `
+            <div class="user-tag">
+            <svg class="users-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M17 11v3l-3-3H8a2 2 0 01-2-2V2c0-1.1.9-2 2-2h10a2 2 0 012 2v7a2 2 0 01-2 2h-1zm-3 2v2a2 2 0 01-2 2H6l-3 3v-3H2a2 2 0 01-2-2V8c0-1.1.9-2 2-2h2v3a4 4 0 004 4h6z"/></svg>
+            <div class="user-name">
+            ${data[eachUser]}
+        </div>
+        </div>
+        `
+    }
+    activeUsersMobile.innerHTML = ''
+    activeUsersMobile.appendChild(div)
 }
 
 function addActiveUser(data) {
