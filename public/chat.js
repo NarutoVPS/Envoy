ef445fd95f108b6f616a788c0a069610eec57443
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-08-23 10:01:38 +0530

    bug: can't send empty msg

--- public/chat.js
+++ public/chat.js
@@ -28,7 +28,7 @@ socket.on('updateActiveUser', data => {
 })
 
 function displayMsg(data) {
-    if (data.userName !== 'undefined' && data.msg !== "") {
+    if (data.userName !== 'undefined' && data.msg.trim() !== "") {
         const div = document.createElement('div')
         div.classList.add('chatMsg')
         div.innerHTML = `<div id="userName">${data.userName} <div class="time">${(data.time).toString()}</div></div>
